550cb28da1b27b8d8fa0ad69d8620ff241100524
shkar822 <83387169+shkar822@users.noreply.github.com>
2021-07-08 10:44:15 +0300

    Update shvan.js

--- shvan.js
+++ shvan.js
@@ -7387,7 +7387,7 @@ message.member.voiceChannel
 
 //تۆكینه‌كان لێره‌ جا بنێ
 
-client.login("ODM1Njg5MjUwODI5NjMxNDg5.YIeupQ.godag4QNNo2hd4btc1i-102XPp4");
+client.login("CIRCULATED USE ANY VPN OR WAIT 30 MINUTES");
 client2.login("");
 client3.login("");
 client4.login("");
